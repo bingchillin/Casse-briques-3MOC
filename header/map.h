b2e2dc452921a441667d006275d44e7c7f821d89
aaldn <aaldn.dev@gmail.com>
2022-11-19 18:28:58 +0100

    Séparation des fonctions liées au map dans les fichiers map.c et map.h

---
+++ header/map.h
@@ -0,0 +1,12 @@
+#include "header.h"
+
+#ifndef CASSE_BRIQUES_3MOC_MAP_H
+#define CASSE_BRIQUES_3MOC_MAP_H
+
+Game *create_map1(Game *game);
+Game *create_map2(Game *game);
+Game *create_map3(Game *game);
+
+void print_map(Game *game);
+
+#endif //CASSE_BRIQUES_3MOC_MAP_H
